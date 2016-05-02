02e7b38ab7bc86c982ce28f7b29dfae657ee1f31
StrauneanuDimitri <strauneanu.dimitri@gmail.com>
2016-05-02 16:39:44 +0300

    Update sendEmail.js

--- cloud/sendEmail.js
+++ cloud/sendEmail.js
@@ -1,22 +1,18 @@
 var api_key = 'key-b932884f8105196fbd78e3dd3304c028';
 var domain = 'sandbox93a83c6dfe1b4404a8ca7f955389701d.mailgun.org';
-var mailgun = require('mailgun')({apiKey: api_key, domain: domain});
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
  
 Parse.Cloud.define("SendEmail", function(request, response) {
-   mailgun.sendEmail({
-    to: request.params.toText,
-    from: request.params.fromText,
-    subject: request.params.subjectText,
-    text: request.params.textText
-}, {
-    success: function(httpResponse) {
-        console.log(httpResponse);
-        response.success("Email sent!");
-    },
-    error: function(httpResponse) {
-       console.error(httpResponse);
-       response.error("Uh oh, something went wrong");
-    }
+
+var data = {
+  from: request.params.fromText,
+  to: request.params.toText,
+  subject: request.params.subjectText,
+  text: request.params.textText
+};
+
+mailgun.messages().send(data, function (error, body) {
+  console.log(body);
 });
  
 });
