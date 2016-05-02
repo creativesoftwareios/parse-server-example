ac3fd9254bb000a382f5a917ac6d502d9db4c17b
StrauneanuDimitri <strauneanu.dimitri@gmail.com>
2016-05-02 16:34:53 +0300

    Update sendEmail.js

--- cloud/sendEmail.js
+++ cloud/sendEmail.js
@@ -1,6 +1,6 @@
 var api_key = 'key-b932884f8105196fbd78e3dd3304c028';
 var domain = 'sandbox93a83c6dfe1b4404a8ca7f955389701d.mailgun.org';
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+var mailgun = require('mailgun')({apiKey: api_key, domain: domain});
  
 Parse.Cloud.define("SendEmail", function(request, response) {
    mailgun.sendEmail({
