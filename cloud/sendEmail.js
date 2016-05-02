47ab50b76e4a1cc4f658b2f80da1e3db527cb6a8
StrauneanuDimitri <strauneanu.dimitri@gmail.com>
2016-05-02 16:18:46 +0300

    Update sendEmail.js

--- cloud/sendEmail.js
+++ cloud/sendEmail.js
@@ -1,1 +1,21 @@
-
+var Mailgun = require('mailgun-js');
+    Mailgun.initialize('sandbox93a83c6dfe1b4404a8ca7f955389701d.mailgun.org', 'key-b932884f8105196fbd78e3dd3304c028');
+ 
+Parse.Cloud.define("SendEmail", function(request, response) {
+   Mailgun.sendEmail({
+    to: request.params.toText,
+    from: request.params.fromText,
+    subject: request.params.subjectText,
+    text: request.params.textText
+}, {
+    success: function(httpResponse) {
+        console.log(httpResponse);
+        response.success("Email sent!");
+    },
+    error: function(httpResponse) {
+       console.error(httpResponse);
+       response.error("Uh oh, something went wrong");
+    }
+});
+ 
+});
