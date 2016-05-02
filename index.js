b1657b6c9e648d9526e259b2b4de9a31cac6e434
StrauneanuDimitri <strauneanu.dimitri@gmail.com>
2016-05-02 10:43:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,26 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'https://emailtest01.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  // Enable email verification
+  verifyUserEmails: true,
+  // The public URL of your app.
+  // This will appear in the link that is used to verify email addresses and reset passwords.
+  // Set the mount path as it is in serverURL
+  publicServerURL: 'https://emailtest01.herokuapp.com/parse',
+  // Your apps name. This will appear in the subject and body of the emails that are sent.
+  appName: 'emailtest01',
+  // The email adapter
+  emailAdapter: {
+    module: 'parse-server-simple-mailgun-adapter',
+    options: {
+      // The address that your emails come from
+      fromAddress: 'parse@example.com',
+      // Your domain from mailgun.com
+      domain: 'sandboxb6015be984e349a4a51038c5e18fc422.mailgun.org',
+      // Your API key from mailgun.com
+      apiKey: 'key-b54a309ea20924558ea6f22a007bb8ee',
+    }
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
