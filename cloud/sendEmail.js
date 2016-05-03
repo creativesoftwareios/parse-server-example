1ef29841c70037565261fc7d225bfef331163390
creativesoftwareios <creativesoftwareios@gmail.com>
2016-05-03 12:02:55 +0300

    Update sendEmail.js

--- cloud/sendEmail.js
+++ cloud/sendEmail.js
@@ -3,6 +3,7 @@ var domain = 'sandbox93a83c6dfe1b4404a8ca7f955389701d.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 var fs = require('fs');
 var path = require('path');
+var _ = require('underscore');
 
 Parse.Cloud.define("SendEmail", function(request, response) {
 
@@ -11,11 +12,18 @@ var filename = 'email_template_html.js';
 var filepath = path.join(__dirname, filename);
 var htmlTemplate = fs.readFileSync(filepath, 'utf8');
 
+var compiled = _.template(htmlTemplate);
+var html = compiled(
+    {
+       'link': request.params.toText
+    }
+);
+
 var data = {
   from: request.params.fromText,
   to: request.params.toText,
   subject: request.params.subjectText,
-  html: htmlTemplate
+  html: html
 };
 
 mailgun.messages().send(data, function (error, body) {
